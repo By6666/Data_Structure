f0ab2063c3e9d6ce6d5126bf2ecd57e247e0c4a8
By <1023695613@qq.com>
2018-11-17 22:12:40 +0800

    create 3_01_Stack

---
+++ Chapter_03/3_01_Stack/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "Stack.h"
+
+int main()
+{
+	Stack_self<int,10> a;
+
+	for (int i = 0; i < 10; i++)  a.Push(i);
+
+	a.PopAll();
+
+	cout << a.isFull() << "  " << a.GetTop() << endl;
+
+	
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
