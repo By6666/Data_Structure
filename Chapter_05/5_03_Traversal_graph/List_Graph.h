ccb8b4ebbf0ccba2698dc0d034dd9d1933970a9d
By <1023695613@qq.com>
2018-11-28 21:00:06 +0800

    create 5_03_Traversal_graph  including dfs and bfs

---
+++ Chapter_05/5_03_Traversal_graph/List_Graph.h
@@ -0,0 +1,97 @@
+﻿#ifndef _LIST_GRAPH
+#define _LIST_GRAPH
+
+#include "Node.h"
+
+template<class T>
+class List_Graph
+{
+public:
+	List_Graph();     //构造函数
+	~List_Graph();    //析构函数
+
+	bool isEmpty() const { return (front == rear) ? true : false; }  //判断是否为空
+	bool isRear() const { return (cur == NULL) ? true : false; }     //判断是否到了结尾
+	void Reset() { pre = cur = front; }                              //复位当前指针
+	Node<T>* GetFront() const { return front; }                      //返回当前头指针
+
+	void insert(const T& data, Node<T>* ptr = NULL);                 //在当前位置之后插入
+
+	void print();     //打印链表全部数据
+
+private:
+	Node<T>* front, *rear;      //存放头节点、尾节点
+	Node<T>* pre, *cur;         //存放当前节点，由插入、删除函数更新
+
+	Node<T>* newNode(const T& data, Node<T>* ptr = NULL);  //产生新的节点
+	void clear();                                          //清空链表，由析构函数调用
+};
+
+template<class T>
+List_Graph<T>::List_Graph()
+{
+	front = new Node<T> (0);
+	pre = cur = rear = front;
+}
+
+template<class T>
+Node<T>* List_Graph<T>::newNode(const T& data, Node<T>* ptr)
+{
+	Node<T>* temp;
+	temp = new Node<T>(data, ptr);
+	return temp;
+}
+
+template<class T>
+void List_Graph<T>::clear()
+{
+	Reset();
+	cur = cur->next;
+	while (!isRear())
+	{
+		pre = cur;
+		cur = cur->next;
+		delete pre;
+		cout << "delete a elem !!" << endl;
+	}
+	rear = pre = cur = front;
+}
+
+template<class T>
+List_Graph<T>::~List_Graph()
+{
+	clear();
+	delete front;
+	cout << "dele front and end !!" << endl;
+}
+
+template<class T>
+void List_Graph<T>::insert(const T& data, Node<T>* ptr)
+{
+	pre = cur;
+	if (cur == rear)
+	{
+		cur = newNode(data, ptr);
+		pre->next = cur;
+		rear = cur;
+	}
+	cur = newNode(data, ptr);
+	pre->next = cur;
+}
+
+template<class T>
+void List_Graph<T>::print()
+{
+	Reset();
+	cur = cur->next;
+	while (!isRear())
+	{
+		pre = cur;
+		cout << pre->data << "  ";
+		cur = cur->next;
+	}
+	cout << endl;
+}
+
+#endif // !_LIST_GRAPH
+
---
+++ Chapter_05/5_03_Traversal_graph/main.cpp
@@ -0,0 +1,132 @@
+﻿//图的遍历,两种方法
+//1、深度优先遍历法，使用堆栈和递归
+//2、广度优先遍历法，使用队列和递归
+
+#include "List_Graph.h"
+#include "Queue_fund.h"
+
+template<class T>
+void dfs(int current); //深度优先遍历法,只需输入起始遍历节点
+
+template<class T>
+void bfs(int current);//广度优先遍历法,只需输入起始遍历节点
+
+#define N 8
+List_Graph<int> a[N];  //产生8个链表头
+int flag_dfs[N],       //深度优先遍历法标志
+    flag_bfs[N];       //广度优先遍历法标志
+
+int main()
+{
+	//原始数据
+	int data[][2] = { {1,2},{2,1},{1,3},{3,1},{2,4},{4,2},{2,5},{5,2},{3,6},{6,3},{3,7},{7,3},{4,5},{5,4},{6,7},{7,6},{5,8},{8,5},{6,8},{8,6} };
+
+	
+
+	for (int i = 0; i < (sizeof(data) / sizeof(data[0])); i++) //更新链表操作
+	{
+		switch (data[i][0])
+		{
+		case 1:  a[0].insert(data[i][1]);
+			break;
+
+		case 2:  a[1].insert(data[i][1]);
+			break;
+
+		case 3:  a[2].insert(data[i][1]);
+			break;
+
+		case 4:  a[3].insert(data[i][1]);
+			break;
+
+		case 5:  a[4].insert(data[i][1]);
+			break;
+
+		case 6:  a[5].insert(data[i][1]);
+			break;
+
+		case 7:  a[6].insert(data[i][1]);
+			break;
+
+		case 8:  a[7].insert(data[i][1]);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	//打印
+	cout << "图的邻接表内容如下：" << endl;
+	for (int i = 0; i < (sizeof(a) / sizeof(List_Graph<int>)); i++)
+	{
+		cout << "第 " << i + 1 << " 个顶点 --->  ";
+		a[i].print();
+	}
+	cout << endl;
+
+
+	cout << "深度优先遍历顶点：" << endl;
+	dfs<int>(0);
+	cout << endl << endl;
+
+
+	cout << "广度优先遍历顶点：" << endl;
+	bfs<int>(0);
+	cout << endl << endl;
+
+
+	system("pause");
+	return 0;
+}
+
+
+//**************深度优先遍历法核心*****************
+template<class T>
+void dfs(int current)
+{
+	Node<T>* ptr;
+
+	flag_dfs[current] = 1;                      //标记遍历了current
+	cout << (current + 1) << "  ";          //打印
+
+	//判断当前链表中的其他节点是否被遍历
+	ptr = a[current].GetFront()->Getnext(); 
+	while (ptr != NULL)
+	{
+		if (flag_dfs[ptr->Getdata() - 1] == 0) dfs<T>(ptr->Getdata() - 1);
+		ptr = ptr->Getnext();
+	}
+}
+//**************************************************
+
+//**************广度优先遍历法核心*****************
+template<class T>
+void bfs(int current)
+{
+	Node<T>* ptr;
+	Queue_fund<T> queue;
+
+	queue.insert(current);
+	flag_bfs[current] = 1;
+	cout << current + 1 << "  ";
+
+	while (!queue.isEmpty())
+	{
+		current = queue.deletefront();
+		ptr = a[current].GetFront()->Getnext();
+
+		while (ptr != NULL)
+		{
+			if (flag_bfs[ptr->Getdata() - 1] == 0)
+			{
+				queue.insert(ptr->Getdata()-1);
+				flag_bfs[ptr->Getdata() - 1] = 1;
+				cout<< ptr->Getdata() << "  ";
+			}
+			ptr = ptr->Getnext();
+		}
+	}
+}
+//**************************************************
+
